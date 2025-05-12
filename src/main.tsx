b72ca2bf71e559520390bb1ef6bddd2aad2473e9
--global <172089930+jdpadillavigo@users.noreply.github.com>
2025-05-12 11:02:52 -0500

    Agregando ruta para Histórico

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainPage from './pages/MainPage'
+import HistoricoPage from './pages/HistoricoPage'
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MainPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <MainPage /> } />
+        <Route path='/historico' element={ <HistoricoPage /> } />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
